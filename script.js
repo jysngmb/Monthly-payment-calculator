efd9bdc684dfa7989aa16b30b7ceb9860cf106e6
jysngmb <jaysongomba@gmail.com>
2024-09-03 13:40:52 +0800

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function goTo (page){
+  document.querySelector("body").style.opacity = 0;
+  setTimeout(function (){
+    document.location.href = page;
+  }, 500);
+};
+
+
+document.addEventListener("DOMCONTENTLoaded", function(){
+  setTimeout(function (){
+    document.querySelector("body").style.opacity = 1;
+  }, 50);
+});
+
 function calculateLoan() {
   loanAmountValue = document.getElementById("loan-amount").value;
 
@@ -12,24 +26,31 @@ function calculateLoan() {
   const totalPayments = MonthsToPayValue * 12;
 
   // monthly payment
-  const monthlyPayment = loanAmountValue * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, totalPayments) / (Math.pow(1 + monthlyInterestRate, totalPayments) - 1).toFixed(2);
+  const monthlyPayment = loanAmountValue * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, totalPayments) / (Math.pow(1 + monthlyInterestRate, totalPayments) - 1);
 
-  document.getElementById(
-    "payment"
-  ).innerHTML = `Monthly payment: ${monthlyPayment}`;
+  document.getElementById("payment").innerHTML = `Monthly payment: ${monthlyPayment.toFixed(2)}`;
+
+  termspayment = monthlyPayment * totalPayments;
+
+  document.getElementById("totalpayment").innerHTML = `Total paymen: ${termspayment.toFixed(2)}`;
+
+  console.log("Monthly Payment: ", monthlyPayment);
 }
 
-console.log("Monthly payment:", monthlyPayment);
 
-function goTo(page) {
-  document.querySelector("body").style.opacity = 0;
-  setTimeout(function () {
-    window.location.href = page;
-  }, 500);
-}
 
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    document.querySelector("body").style.opacity = 1;
-  }, 50);
-});
+// function calculateLoan(){
+//   loanAmountValue = document.getElementById("loan-amount").value;
+//   interestRateValue = document.getElementById("interest-rate").value;
+//   MonthsToPayValue = document.getElementById("months-pay").value;
+
+//   const monthlyInterestRate = interestRateValue / 12 / 100;
+//   const totalPayments = MonthsToPayValue * 12;
+//   const monthlyPayment = loanAmountValue * monthlyInterestRate * (1 + monthlyInterestRate, totalPayments)^60 / (1 + monthlyInterestRate, totalPayments - 1)^60.toFixed(2));
+
+//   document.getElementById("payment").innerHTML = `Monthly payment: ${monthlyPayment}`;
+// }
+
+
+
+
